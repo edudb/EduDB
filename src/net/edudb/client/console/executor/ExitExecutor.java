4adc7b122d57c1b251eb4c84ede5e10882d5d55a
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2016-05-25 20:06:00 +0200

    Merged the client code with the server

---
+++ src/net/edudb/client/console/executor/ExitExecutor.java
@@ -0,0 +1,36 @@
+/*
+EduDB is made available under the OSI-approved MIT license.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package net.edudb.client.console.executor;
+
+import net.edudb.client.Client;
+import net.edudb.client.ClientWriter;
+
+public class ExitExecutor implements ConsoleExecutorChain {
+	private ConsoleExecutorChain nextElement;
+
+	@Override
+	public void setNextInChain(ConsoleExecutorChain chainElement) {
+		this.nextElement = chainElement;
+	}
+
+	@Override
+	public void execute(String string) {
+		if (string.equalsIgnoreCase("exit")) {
+			ClientWriter.getInstance().write(string);
+			Client.getInstance().getChannel().close();
+			Client.getInstance().getHandler().setReceiving(false);
+			System.exit(0);
+			return;
+		}
+		nextElement.execute(string);
+	}
+
+}
