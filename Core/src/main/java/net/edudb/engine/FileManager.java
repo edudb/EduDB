a725108f288721f9272ddf8376cf93bd5006f74d
AhmedNasserG <ahmednasser217217@gmail.com>
2023-03-06 15:34:10 +0200

    refactor: FileManager

--- Core/src/main/java/net/edudb/engine/FileManager.java
+++ Core/src/main/java/net/edudb/engine/FileManager.java
@@ -1,12 +1,11 @@
 /*
-EduDB is made available under the OSI-approved MIT license.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ *
+ * EduDB is made available under the OSI-approved MIT license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * /
+ */
 
 package net.edudb.engine;
 
@@ -14,15 +13,16 @@ import net.edudb.block.*;
 import net.edudb.page.Page;
 import net.edudb.structure.table.*;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class FileManager {
     private static final FileManager instance = new FileManager();
-    private static String dataDirectory;
-    private static String schema;
-    private static boolean initialized;
-    private static boolean isWindows;
 
     private FileManager() {
     }
@@ -31,106 +31,48 @@ public class FileManager {
         return instance;
     }
 
-    public static ArrayList<String> readFile(String file) {
-        if (!initialized) {
-            init();
-        }
+    public static ArrayList<String> readSchema() {
+        return readFile(getSchemaPath());
+    }
+
+    public static String getSchemaPath() {
+        return Utility.appendToPath(Config.databasePath(), "schema.txt");
+    }
+
+    /**
+     * Reads all lines from a file and returns them as a list of strings.
+     *
+     * @param filePath the path to the file to read
+     * @return a list of strings containing all lines from the file, or an empty list if an error occurs
+     * @author Ahmed Nasser Gaafar
+     */
+    public static ArrayList<String> readFile(String filePath) {
         ArrayList<String> lines = new ArrayList<>();
         try {
-            File dataFile = new File(file);
-            if (!dataFile.exists()) {
-                dataFile.createNewFile();
-            }
-            BufferedReader br = new BufferedReader(new FileReader(file));
-            String line = br.readLine();
-            while (line != null) {
-                lines.add(line);
-                line = br.readLine();
-            }
-            br.close();
+            lines = (ArrayList<String>) Files.readAllLines(Paths.get(filePath));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.err.println("Error reading file: " + e.getMessage());
         }
         return lines;
     }
 
-    public static void init() {
-        if (initialized) {
-            return;
-        }
-        // dataDirectory = appendToPath(Config.absolutePath(), "database");
-        dataDirectory = Config.databasePath();
-        // File file = new File(dataDirectory);
-        // if (!file.exists()) {
-        // file.mkdir();
-        // }
-        String operatingSystem = System.getProperty("os.name").toLowerCase();
-        isWindows = operatingSystem.startsWith("windows");
-        schema = appendToPath(dataDirectory, "schema.txt");
-        initialized = true;
-    }
-
-    public static String getSchema() {
-        init();
-        return appendToPath(Config.databasePath(), "schema.txt");
-    }
-
-    private static String appendToPath(String S1, String S2) {
-        if (isWindows) {
-            return S1 + "\\" + S2;
-        } else {
-            return S1 + "/" + S2;
-        }
-    }
-
-    public static void addToFile(String file, String text) {
+    /**
+     * Writes data to a file.
+     *
+     * @param filePath the path to the file to write to
+     * @param data     the data to write to the file
+     * @param append   true to append the data to the end of the file, false to overwrite the file
+     * @author Ahmed Nasser Gaafar
+     */
+    public static void writeFile(String filePath, String data, boolean append) {
         try {
-            File dataFile = new File(file);
-            if (!dataFile.exists()) {
-                dataFile.createNewFile();
+            File file = new File(filePath);
+            if (!file.exists()) {
+                file.createNewFile();
             }
-            BufferedWriter output = new BufferedWriter(new FileWriter(file, true));
-            output.append(text);
-            output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static void createTable(String tableName) {
-        try {
-            String dir = appendToPath(dataDirectory, tableName);
-            File dataFile = new File(dir);
-            if (!dataFile.exists()) {
-                dataFile.mkdir();
-                File table = new File(appendToPath(dir, (tableName + ".txt")));
-                table.createNewFile();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static String getTable(String tableName) {
-        String table;
-        try {
-            String dir = appendToPath(dataDirectory, tableName);
-            table = appendToPath(dir, (tableName + ".txt"));
-            return table;
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static void writeToFile(String data, String fileName) {
-        try {
-            File dataFile = new File(fileName);
-            dataFile.delete();
-            dataFile.createNewFile();
-            BufferedWriter output = new BufferedWriter(new FileWriter(fileName, true));
-            output.write(data);
-            output.close();
+            BufferedWriter writer = new BufferedWriter(new FileWriter(file, append));
+            writer.write(data);
+            writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -147,8 +89,7 @@ public class FileManager {
         BlockReader blockReader = blockFactory.getReader(Config.blockType());
 
         try {
-            Page page = blockReader.read(pageName);
-            return page;
+            return blockReader.read(pageName);
         } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
@@ -182,8 +123,7 @@ public class FileManager {
         TableReader tableReader = tableFactory.getReader(Config.tableType());
 
         try {
-            Table table = tableReader.read(tableName);
-            return table;
+            return tableReader.read(tableName);
         } catch (ClassNotFoundException | IOException e) {
             e.printStackTrace();
         }
--- Core/src/main/java/net/edudb/statistics/Schema.java
+++ Core/src/main/java/net/edudb/statistics/Schema.java
@@ -1,147 +1,146 @@
 /*
-EduDB is made available under the OSI-approved MIT license.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ *
+ * EduDB is made available under the OSI-approved MIT license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * /
+ */
 
 package net.edudb.statistics;
 
+import com.google.common.collect.Iterables;
 import net.edudb.engine.FileManager;
 import net.edudb.structure.Column;
 import net.edudb.structure.table.Table;
 import net.edudb.structure.table.TableManager;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Set;
 
-import com.google.common.collect.Iterables;
-
 /**
  * Created by mohamed on 4/1/14.
  */
 public class Schema {
 
-	private static final Schema instance = new Schema();
-	private final HashMap<String, ArrayList<String>> schema;
+    private static final Schema instance = new Schema();
+    private final HashMap<String, ArrayList<String>> schema;
 
-	private Schema() {
-		schema = new HashMap<>();
-		setSchema();
-	}
+    private Schema() {
+        schema = new HashMap<>();
+        setSchema();
+    }
 
-	public static Schema getInstance() {
-		return instance;
-	}
+    public static Schema getInstance() {
+        return instance;
+    }
 
-	/**
-	 * Checks the availability of the given table in the database's schema.
-	 *
-	 * @param tableName
-	 *            The name of the table to check.
-	 * @return The availability of the table.
-	 */
-	public boolean chekTableExists(String tableName) {
-		return schema.get(tableName) != null;
-	}
+    /**
+     * Checks the availability of the given table in the database's schema.
+     *
+     * @param tableName The name of the table to check.
+     * @return The availability of the table.
+     */
+    public boolean chekTableExists(String tableName) {
+        return schema.get(tableName) != null;
+    }
 
-	public void setSchema() {
-		ArrayList<String> lines = FileManager.readFile(FileManager.getSchema());
-		for (String line : lines) {
-			putTable(line);
-		}
-	}
+    public void setSchema() {
+        ArrayList<String> lines = FileManager.readSchema();
+        for (String line : lines) {
+            putTable(line);
+        }
+    }
 
-	/**
-	 * Returns the columns of the required table.
-	 *
-	 * @param tableName
-	 *            The name of the required table.
-	 * @return Columns of the required table as an {@link ArrayList}.
-	 */
-	public ArrayList<Column> getColumns(String tableName) {
-		ArrayList<Column> columns = new ArrayList<>();
-		Table table = TableManager.getInstance().read(tableName);
-		LinkedHashMap<String, String> columnTypes = table.getColumnTypes();
-		for (int i = 1; i <= columnTypes.size(); i++) {
-			String columnName = Iterables.get(columnTypes.keySet(), i - 1);
-			String columnType = Iterables.get(columnTypes.values(), i - 1);
-			columns.add(new Column(i, columnName, tableName, columnType));
-		}
+    /**
+     * Returns the columns of the required table.
+     *
+     * @param tableName The name of the required table.
+     * @return Columns of the required table as an {@link ArrayList}.
+     */
+    public ArrayList<Column> getColumns(String tableName) {
+        ArrayList<Column> columns = new ArrayList<>();
+        Table table = TableManager.getInstance().read(tableName);
+        LinkedHashMap<String, String> columnTypes = table.getColumnTypes();
+        for (int i = 1; i <= columnTypes.size(); i++) {
+            String columnName = Iterables.get(columnTypes.keySet(), i - 1);
+            String columnType = Iterables.get(columnTypes.values(), i - 1);
+            columns.add(new Column(i, columnName, tableName, columnType));
+        }
 
-		return columns;
-	}
+        return columns;
+    }
 
-	// add table to schema object
-	private void putTable(String line) {
-		String[] tokens = line.split(" ");
-		String TableName = tokens[0];
-		ArrayList<String> columns = new ArrayList<String>();
-		for (int i = 1; i < tokens.length; i += 2) {
-			String columnName = tokens[i];
-			columns.add(columnName);
-		}
-		schema.put(TableName, columns);
-	}
+    // add table to schema object
+    private void putTable(String line) {
+        String[] tokens = line.split(" ");
+        String TableName = tokens[0];
+        ArrayList<String> columns = new ArrayList<String>();
+        for (int i = 1; i < tokens.length; i += 2) {
+            String columnName = tokens[i];
+            columns.add(columnName);
+        }
+        schema.put(TableName, columns);
+    }
 
-	// add table to schema file
-	public void addTable(String line) {
-		putTable(line);
-		line += System.lineSeparator();
-		FileManager.addToFile(FileManager.getSchema(), line);
-	}
+    // add table to schema file
+    public void addTable(String line) {
+        putTable(line);
+        line += System.lineSeparator();
+        FileManager.writeFile(FileManager.getSchemaPath(), line, true);
+    }
 
-	public void removeTable(String tableName) {
+    public void removeTable(String tableName) {
 
-		ArrayList<String> schema = FileManager.readFile(FileManager.getSchema());
+        ArrayList<String> schema = FileManager.readSchema();
 
-		String schemaData = "";
+        String schemaData = "";
 
-		for (String string : schema) {
-			if (!string.startsWith(tableName)) {
-				schemaData += string + "\r\n";
-			}
-		}
+        for (String string : schema) {
+            if (!string.startsWith(tableName)) {
+                schemaData += string + "\r\n";
+            }
+        }
 
-		File file = new File(FileManager.getSchema());
-		if (file.exists()) {
-			file.delete();
-		}
+        File file = new File(FileManager.getSchemaPath());
+        if (file.exists()) {
+            file.delete();
+        }
 
-		this.schema.remove(tableName);
+        this.schema.remove(tableName);
 
-		FileManager.writeToFile(schemaData, FileManager.getSchema());
-	}
+        FileManager.writeFile(FileManager.getSchemaPath(), schemaData, false);
+    }
 
-	public HashMap<String, ArrayList<String>> getSchema() {
-		return schema;
-	}
+    public HashMap<String, ArrayList<String>> getSchema() {
+        return schema;
+    }
 
-	public int getCount(String tableName) {
-		return schema.get(tableName).size();
-	}
+    public int getCount(String tableName) {
+        return schema.get(tableName).size();
+    }
 
-	public ArrayList<String> getColumnNames(String tableName) {
-		ArrayList<String> columnNames = new ArrayList<>();
-		int count = schema.get(tableName).size();
-		for (int i = 0; i < count; i++) {
-			columnNames.add(schema.get(tableName).get(i));
-		}
-		return columnNames;
-	}
+    public ArrayList<String> getColumnNames(String tableName) {
+        ArrayList<String> columnNames = new ArrayList<>();
+        int count = schema.get(tableName).size();
+        for (int i = 0; i < count; i++) {
+            columnNames.add(schema.get(tableName).get(i));
+        }
+        return columnNames;
+    }
 
-	public int getColumnNumber(String name, String tableName) {
-		return getColumnNames(tableName).indexOf(name);
-	}
+    public int getColumnNumber(String name, String tableName) {
+        return getColumnNames(tableName).indexOf(name);
+    }
 
-	public Set<String> getTableNames() {
-		return schema.keySet();
-	}
+    public Set<String> getTableNames() {
+        return schema.keySet();
+    }
 
-	public void resetSchema() { schema.clear(); }
+    public void resetSchema() {
+        schema.clear();
+    }
 }
