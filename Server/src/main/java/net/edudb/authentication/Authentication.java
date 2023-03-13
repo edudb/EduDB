056adb8508725fd7b9504b6f93003585da74e94a
AhmedNasserG <ahmednasser217217@gmail.com>
2023-03-13 21:54:17 +0200

    feat: add Authentication class

---
+++ Server/src/main/java/net/edudb/authentication/Authentication.java
@@ -0,0 +1,84 @@
+/*
+ *
+ * EduDB is made available under the OSI-approved MIT license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * /
+ */
+
+package net.edudb.authentication;
+
+import net.edudb.exceptions.AuthenticationFailedException;
+import net.edudb.exceptions.InvalidRoleException;
+import net.edudb.exceptions.UserAlreadyExistException;
+import net.edudb.exceptions.UserNotFoundException;
+
+public class Authentication {
+    private static UserManager userManager = UserManager.getInstance();
+    private static final int USERNAME_INDEX = 0;
+    private static final int PASSWORD_INDEX = 1;
+    private static final int ROLE_INDEX = 2;
+
+    private Authentication() {
+        throw new IllegalStateException("Utility class");
+    }
+
+
+    /**
+     * Creates a new user with the given username, password and role.
+     *
+     * @param username Username of the user to be created.
+     * @param password Password of the user to be created.
+     * @param role     Role of the user to be created. limited to {@link UserRole} values.
+     * @auther Ahmed Nasser Gaafar
+     */
+    public static void createUser(String username, String password, UserRole role) throws UserAlreadyExistException {
+        String hashedPassword = PasswordUtil.hashPassword(password);
+        try {
+            userManager.writeUser(username, hashedPassword, role.toString());
+        } catch (InvalidRoleException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Removes a user with the given username.
+     *
+     * @param username Username of the user to be removed.
+     * @throws UserNotFoundException If the user is not found.
+     */
+    public static void removeUser(String username) throws UserNotFoundException {
+        userManager.removeUser(username);
+    }
+
+    /**
+     * Authenticates a user with the given username and password.
+     *
+     * @param username
+     * @param password
+     * @return A JWT token if the user is authenticated.
+     * @throws AuthenticationFailedException If the user is not authenticated.
+     * @auther Ahmed Nasser Gaafar
+     */
+    public static String login(String username, String password) throws AuthenticationFailedException {
+        try {
+            String[] user = userManager.readUser(username);
+            if (!PasswordUtil.verifyPassword(password, user[PASSWORD_INDEX])) {
+                throw new AuthenticationFailedException("Password is incorrect");
+            }
+            UserRole role;
+            try {
+                role = UserRole.fromString(user[ROLE_INDEX]);
+            } catch (InvalidRoleException e) {
+                System.err.println(String.format("Data of user %s is corrupted", username));
+                throw new RuntimeException(e);
+            }
+
+            return JwtUtil.generateToken(username, role);
+
+        } catch (UserNotFoundException e) {
+            throw new AuthenticationFailedException("Invalid username");
+        }
+    }
+}
---
+++ Server/src/test/java/net/edudb/authentication/AuthenticationTest.java
@@ -0,0 +1,89 @@
+/*
+ *
+ * EduDB is made available under the OSI-approved MIT license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * /
+ */
+
+package net.edudb.authentication;
+
+import net.edudb.engine.Config;
+import net.edudb.engine.DatabaseSystem;
+import net.edudb.exceptions.AuthenticationFailedException;
+import net.edudb.exceptions.UserAlreadyExistException;
+import net.edudb.exceptions.UserNotFoundException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileWriter;
+import java.util.List;
+
+public class AuthenticationTest {
+    private static final String USER_NAME = "test_username";
+    private static final String PASSWORD = "password";
+    private static final String WRONG_PASSWORD = "wrong_password";
+    private static final UserRole ROLE = UserRole.USER;
+
+    @BeforeAll
+    public static void setup() {
+        DatabaseSystem.getInstance().initializeDirectories();
+    }
+
+    @AfterEach
+    public void tearDown() {
+        try {
+            FileWriter writer = new FileWriter(Config.usersPath());
+            writer.write("");
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void createUser() throws UserAlreadyExistException {
+        Authentication.createUser(USER_NAME, PASSWORD, ROLE);
+        List<String[]> users = UserManager.getInstance().readAllUsers();
+        Assertions.assertEquals(1, users.size());
+        Assertions.assertEquals(USER_NAME, users.get(0)[0]);
+        Assertions.assertEquals(ROLE.toString(), users.get(0)[2]);
+        Assertions.assertTrue(PasswordUtil.verifyPassword(PASSWORD, users.get(0)[1]));
+    }
+
+    @Test
+    public void createDuplicatedUser() throws UserAlreadyExistException {
+        Authentication.createUser(USER_NAME, PASSWORD, ROLE);
+        Assertions.assertThrows(UserAlreadyExistException.class, () -> Authentication.createUser(USER_NAME, PASSWORD, ROLE));
+    }
+
+    @Test
+    void loginWithCorrectPassword() throws AuthenticationFailedException, UserAlreadyExistException {
+        Authentication.createUser(USER_NAME, PASSWORD, ROLE);
+        String token = Authentication.login(USER_NAME, PASSWORD);
+        Assertions.assertNotNull(token);
+    }
+
+    @Test
+    void loginWithWrongPassword() throws UserAlreadyExistException {
+        Authentication.createUser(USER_NAME, PASSWORD, ROLE);
+        Assertions.assertThrows(AuthenticationFailedException.class, () -> Authentication.login(USER_NAME, WRONG_PASSWORD));
+
+    }
+
+    @Test
+    void removeExistingUser() throws UserNotFoundException, UserAlreadyExistException {
+        Authentication.createUser(USER_NAME, PASSWORD, ROLE);
+        Authentication.removeUser(USER_NAME);
+        List<String[]> users = UserManager.getInstance().readAllUsers();
+        Assertions.assertEquals(0, users.size());
+    }
+
+    @Test
+    void removeNonExistingUser() {
+        Assertions.assertThrows(UserNotFoundException.class, () -> Authentication.removeUser(USER_NAME));
+    }
+}
