1088d63e68ef1f6a208a871f9ca9af602cb02d22
AhmedNasserG <ahmednasser217217@gmail.com>
2023-03-19 12:07:15 +0200

    refactor: file manager

--- Core/src/main/java/net/edudb/engine/FileManager.java
+++ Core/src/main/java/net/edudb/engine/FileManager.java
@@ -10,16 +10,19 @@
 package net.edudb.engine;
 
 import net.edudb.block.*;
+import net.edudb.exception.*;
 import net.edudb.page.Page;
-import net.edudb.structure.table.*;
+import net.edudb.structure.table.Table;
+import net.edudb.structure.table.TableAbstractFactory;
+import net.edudb.structure.table.TableReader;
+import net.edudb.structure.table.TableReaderFactory;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.FileAlreadyExistsException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 
 public class FileManager {
     private static final FileManager instance = new FileManager();
@@ -32,7 +35,11 @@ public class FileManager {
     }
 
     public static ArrayList<String> readSchema() {
-        return readFile(Config.schemaPath());
+        try {
+            return readFile(Config.schemaPath());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     /**
@@ -42,14 +49,14 @@ public class FileManager {
      * @return a list of strings containing all lines from the file, or an empty list if an error occurs
      * @author Ahmed Nasser Gaafar
      */
-    public static ArrayList<String> readFile(String filePath) {
+    public static ArrayList<String> readFile(String filePath) throws FileNotFoundException {
         ArrayList<String> lines = new ArrayList<>();
         try {
             lines = (ArrayList<String>) Files.readAllLines(Paths.get(filePath));
         } catch (IOException e) {
-            System.err.println("Error reading file: " + e.getMessage());
-            e.printStackTrace();
+            throw new FileNotFoundException(String.format("(%s) is not found", filePath));
         }
+        lines.removeIf(String::isEmpty);
         return lines;
     }
 
@@ -68,7 +75,7 @@ public class FileManager {
                 file.createNewFile();
             }
             BufferedWriter writer = new BufferedWriter(new FileWriter(file, append));
-            writer.write(data);
+            writer.write(data + System.lineSeparator());
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
@@ -127,19 +134,213 @@ public class FileManager {
         return null;
     }
 
-    /**
-     * Writes a table to disk using a table writer.
-     *
-     * @param table The table to write.
-     */
-    public void writeTable(Table table) {
-        TableAbstractFactory tableFactory = new TableWriterFactory();
-        TableWriter blockWriter = tableFactory.getWriter(Config.tableType());
 
+    /**
+     * @param path the path to the directory to list
+     * @return an array of strings containing the names of the subdirectories of the given directory
+     * @throws DirectoryNotFoundException if the given directory does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public String[] listDirectories(String path) throws DirectoryNotFoundException {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            throw new DirectoryNotFoundException(String.format("(%s) is not found", path));
+        }
+        File[] subDirectories = directory.listFiles(File::isDirectory);
+        String[] subDirectoriesNames = new String[subDirectories.length];
+        for (int i = 0; i < subDirectories.length; i++) {
+            subDirectoriesNames[i] = subDirectories[i].getName();
+        }
+        return subDirectoriesNames;
+    }
+
+    /**
+     * @param path the path to the directory to list
+     * @return an array of strings containing the names of the files of the given directory without the extension
+     * @throws DirectoryNotFoundException if the given directory does not exist
+     */
+    public String[] listFiles(String path) throws DirectoryNotFoundException {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            throw new DirectoryNotFoundException(String.format("(%s) is not found", path));
+        }
+        File[] files = directory.listFiles(File::isFile);
+        String[] filesNames = new String[files.length];
+        for (int i = 0; i < files.length; i++) {
+            String fileName = files[i].getName();
+            filesNames[i] = fileName.split("\\.")[0];
+        }
+        return filesNames;
+    }
+
+    /**
+     * @return an array of strings containing the names of the workspaces
+     * @throws DirectoryNotFoundException if the workspaces directory does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public String[] listWorkspaces() throws DirectoryNotFoundException {
+        return listDirectories(Config.workspacesPath());
+    }
+
+    /**
+     * @param workspaceName the name of the workspace to list its databases
+     * @return an array of strings containing the names of the databases in the given workspace
+     * @throws DirectoryNotFoundException if the given workspace does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public String[] listDatabases(String workspaceName) throws DirectoryNotFoundException {
+        return listDirectories(Config.databasesPath(workspaceName));
+    }
+
+    /**
+     * @param workspaceName the name of the workspace that contains the database to list its tables
+     * @param databaseName  the name of the database to list its tables
+     * @return an array of strings containing the names of the tables in the given database
+     * @throws DirectoryNotFoundException if the given workspace or database does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public String[] listTables(String workspaceName, String databaseName) throws DirectoryNotFoundException {
+        return listFiles(Config.tablesPath(workspaceName, databaseName));
+    }
+
+    /**
+     * @param workspaceName the name of the workspace that contains the database to list its pages
+     * @param databaseName  the name of the database to list its pages
+     * @return an array of strings containing the names of the pages in the given database
+     * @throws FileNotFoundException if the given workspace or database does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public List<String> readSchemaFile(String workspaceName, String databaseName) throws FileNotFoundException {
+        return readFile(Config.schemaPath(workspaceName, databaseName));
+    }
+
+    /**
+     * @param path the path of the directory to create
+     * @throws DirectoryAlreadyExistsException if the given directory already exists
+     * @author Ahmed Nasser Gaafar
+     */
+    public void createDirectory(String path) throws DirectoryAlreadyExistsException {
+        File directory = new File(path);
+        if (directory.exists()) {
+            throw new DirectoryAlreadyExistsException(String.format("(%s) already exists", path));
+        }
+        directory.mkdirs();
+    }
+
+    /**
+     * @param path the path of the directory to create if not exists
+     * @author Ahmed Nasser Gaafar
+     */
+    public void createDirectoryIfNotExists(String path) {
         try {
-            blockWriter.write(table);
+            createDirectory(path);
+        } catch (DirectoryAlreadyExistsException e) {
+            // do nothing
+        }
+
+    }
+
+    /**
+     * @param path the path of the directory to delete
+     * @throws DirectoryNotFoundException if the given directory does not exist
+     * @author Ahmed Nasser Gaafar
+     */
+    public void deleteDirectory(String path) throws DirectoryNotFoundException {
+        File directory = new File(path);
+        if (!directory.exists()) {
+            throw new DirectoryNotFoundException(String.format("(%s) is not found", path));
+        }
+        deleteDirectoryRecursively(directory);
+    }
+
+    /**
+     * @param directory the directory to delete recursively
+     * @author Ahmed Nasser Gaafar
+     */
+    private void deleteDirectoryRecursively(File directory) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteDirectoryRecursively(file);
+                } else {
+                    file.delete();
+                }
+            }
+        }
+        directory.delete();
+    }
+
+    public void createFile(String path) throws FileAlreadyExistsException {
+        File file = new File(path);
+        if (file.exists()) {
+            throw new FileAlreadyExistsException(String.format("(%s) already exists", path));
+        }
+        try {
+            file.createNewFile();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void deleteFile(String path) throws FileNotFoundException {
+        File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(String.format("(%s) is not found", path));
+        }
+        file.delete();
+    }
+
+    public void appendLineToFile(String path, String line) throws FileNotFoundException {
+        if (!new File(path).exists()) {
+            throw new FileNotFoundException(String.format("(%s) is not found", path));
+        }
+        writeFile(path, line, true);
+    }
+
+    public void removeLineFromFileWithPrefix(String path, String prefix) throws FileNotFoundException {
+        List<String> lines = readFile(path);
+        StringBuilder newFileData = new StringBuilder();
+        for (String line : lines) {
+            if (line.startsWith(prefix)) continue;
+            newFileData.append(line);
+        }
+        writeFile(path, newFileData.toString(), false);
+    }
+
+    public void createWorkspace(String workspaceName) throws WorkspaceAlreadyExistException {
+        try {
+            createDirectory(Config.workspacePath(workspaceName));
+            createDirectory(Config.databasesPath(workspaceName));
+        } catch (DirectoryAlreadyExistsException e) {
+            throw new WorkspaceAlreadyExistException(String.format("workspace (%s) already exists", workspaceName), e);
+        }
+    }
+
+    public void deleteWorkspace(String workspaceName) throws WorkspaceNotFoundException {
+        try {
+            deleteDirectory(Config.workspacePath(workspaceName));
+        } catch (DirectoryNotFoundException e) {
+            throw new WorkspaceNotFoundException(String.format("workspace (%s) is not found", workspaceName), e);
+        }
+    }
+
+    public void createDatabase(String workspaceName, String databaseName) throws DatabaseAlreadyExistException {
+        try {
+            createDirectory(Config.databasePath(workspaceName, databaseName));
+            createDirectory(Config.tablesPath(workspaceName, databaseName));
+            createDirectory(Config.pagesPath(workspaceName, databaseName));
+            createFile(Config.schemaPath(workspaceName, databaseName));
+        } catch (DirectoryAlreadyExistsException | FileAlreadyExistsException e) {
+            throw new DatabaseAlreadyExistException(String.format("database (%s) already exists", databaseName), e);
+        }
+    }
+
+    public void deleteDatabase(String workspaceName, String databaseName) throws DatabaseNotFoundException {
+        try {
+            deleteDirectory(Config.databasePath(workspaceName, databaseName));
+        } catch (DirectoryNotFoundException e) {
+            throw new DatabaseNotFoundException(String.format("database (%s) is not found", databaseName), e);
         }
     }
 
