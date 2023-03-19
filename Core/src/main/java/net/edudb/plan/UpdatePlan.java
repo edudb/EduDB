054908c04b3549d7bb95d8ec4588ca87de7283e8
AhmedNasserG <ahmednasser217217@gmail.com>
2023-03-19 14:02:06 +0200

    fix: minor updates

--- Core/src/main/java/net/edudb/plan/UpdatePlan.java
+++ Core/src/main/java/net/edudb/plan/UpdatePlan.java
@@ -1,12 +1,11 @@
 /*
-EduDB is made available under the OSI-approved MIT license.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ *
+ * EduDB is made available under the OSI-approved MIT license.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * /
+ */
 
 package net.edudb.plan;
 
@@ -27,58 +26,57 @@ import net.edudb.statistics.Schema;
  * A plan to update data in a table.
  *
  * @author Ahmed Abdul Badie
- *
  */
 public class UpdatePlan extends Plan {
 
-	@Override
-	public QueryTree makePlan(SQLStatement sqlStatement) {
-		SQLUpdateStatement statement = (SQLUpdateStatement) sqlStatement;
+    @Override
+    public QueryTree makePlan(SQLStatement sqlStatement) {
+        SQLUpdateStatement statement = (SQLUpdateStatement) sqlStatement;
 
-		if (!Schema.getInstance().chekTableExists(statement.getTableName())) {
-			return null;
-		}
+        if (!Schema.getInstance().checkTableExists(statement.getTableName())) {
+            return null;
+        }
 
-		QueryTree plan = null;
+        QueryTree plan = null;
 
-		UpdateTableOperator operator = new UpdateTableOperator();
+        UpdateTableOperator operator = new UpdateTableOperator();
 
-		RelationOperatorParameter parameter = new RelationOperatorParameter(statement.getTableName());
+        RelationOperatorParameter parameter = new RelationOperatorParameter(statement.getTableName());
 
-		/**
-		 * Creates a relation operator to be the child of the update operator.
-		 */
-		RelationOperator relation = new RelationOperator();
-		relation.setParameter(parameter);
-		operator.setChild(relation);
+        /**
+         * Creates a relation operator to be the child of the update operator.
+         */
+        RelationOperator relation = new RelationOperator();
+        relation.setParameter(parameter);
+        operator.setChild(relation);
 
-		ExpressionTree expressionTree = null;
-		if (statement.getWhereClause() != null) {
-			/**
-			 * This block of code is used to get the expression tree from the
-			 * update statement's where clause.
-			 *
-			 * Since there is no other way to do it, this is just a "hack" to
-			 * get things going.
-			 *
-			 * This create a query tree that has only one node, filter node,
-			 * that has an expression tree as its parameter. This expression
-			 * tree is used as a parameter to the update table operator.
-			 */
-			Translator translator = new Translator();
-			String ra = translator
-					.translate("select * from " + statement.getTableName() + " " + statement.getWhereClause());
-			QueryTree queryTree = translator.processRelationalAlgebra(ra);
-			FilterOperator filter = (FilterOperator) queryTree.getRoot();
-			expressionTree = (BinaryExpressionTree) filter.getParameter();
-		}
+        ExpressionTree expressionTree = null;
+        if (statement.getWhereClause() != null) {
+            /**
+             * This block of code is used to get the expression tree from the
+             * update statement's where clause.
+             *
+             * Since there is no other way to do it, this is just a "hack" to
+             * get things going.
+             *
+             * This create a query tree that has only one node, filter node,
+             * that has an expression tree as its parameter. This expression
+             * tree is used as a parameter to the update table operator.
+             */
+            Translator translator = new Translator();
+            String ra = translator
+                    .translate("select * from " + statement.getTableName() + " " + statement.getWhereClause());
+            QueryTree queryTree = translator.processRelationalAlgebra(ra);
+            FilterOperator filter = (FilterOperator) queryTree.getRoot();
+            expressionTree = (BinaryExpressionTree) filter.getParameter();
+        }
 
-		UpdateTableOperatorParameter updateParameter = new UpdateTableOperatorParameter(statement.getAssignemnts(),
-				expressionTree, statement.getTableName());
-		operator.setParameter(updateParameter);
+        UpdateTableOperatorParameter updateParameter = new UpdateTableOperatorParameter(statement.getAssignemnts(),
+                expressionTree, statement.getTableName());
+        operator.setParameter(updateParameter);
 
-		plan = new QueryTree(operator);
+        plan = new QueryTree(operator);
 
-		return plan;
-	}
+        return plan;
+    }
 }
